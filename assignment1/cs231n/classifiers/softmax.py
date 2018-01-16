b8681d8be25a768d8efa698ce8caca9ce6b1897f
lianxiaolei <lianxiaolei@xueleyun.com>
2018-01-16 14:21:12 +0800

    'softmax_done'

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -35,7 +35,7 @@ def softmax_loss_naive(W, X, y, reg):
         loss_i = -shift_scores[y[i]] + np.log(sum(np.exp(shift_scores)))
         loss += loss_i
         for j in xrange(num_class):
-            softmax_output = np.exp(shift_scores[j])/sum(np.exp(shift_scores))
+            softmax_output = np.exp(shift_scores[j]) / sum(np.exp(shift_scores))
             if j == y[i]:
                 dW[:, j] += (-1 + softmax_output) * X[i]
             else:
@@ -54,8 +54,18 @@ def softmax_loss_vectorized(W, X, y, reg):
 
     Inputs and outputs are the same as softmax_loss_naive.
     """
+    num_train, num_feature = X.shape
+    num_class = W.shape[1]
     loss = 0.0
     dW = np.zeros_like(W)
     scores = X.dot(W)
-    shift_scores = scores - max(scores)
-    loss = np.sum()
+    shift_scores = scores - np.max(scores, axis=1).reshape(-1, 1)
+    softmax_output = np.exp(shift_scores) / np.sum(np.exp(shift_scores), axis=1).reshape(-1, 1)
+    loss = -np.sum(np.log(softmax_output[range(num_train), list(y)]))
+    loss /= num_train
+    loss += 0.5 * reg * np.sum(W * W)
+    dS = softmax_output.copy()
+    dS[range(num_train), list(y)] -= 1
+    dW = X.T.dot(dS)
+    dW = dW / num_train + reg * W
+    return loss, dW
